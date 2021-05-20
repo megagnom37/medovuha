9f87f51c6e534ab1eb95fb2c703cc423eb272ba3
saha223311 <saha_97@bk.ru>
2021-05-20 22:40:05 +0300

    Added ServerConnector.

--- Python/medovuha/server.py
+++ Python/medovuha/server.py
@@ -4,50 +4,60 @@ import time
 import pickle
 
 
-host = '127.0.0.1'
-port = 9999
+class Server:
+    def __init__(self, host, port):
+        self._host = host
+        self._port = port
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((host, port))
-sock.settimeout(4.0)
+        self._cli_addr = set()
+        self._cli_data = dict()
 
-cli_addr = set()
-cli_data = dict()
+        self._sock = None
 
-def handle_data(data, from_address):
-    player_id, x, y, z = data.decode().split(':')
-    # print(f'\tPlayer: {player_id} ({x}, {y}, {z})')
-    cli_addr.add(from_address)
-    cli_data[from_address] = f'{player_id}:{x}:{y}:{z}'
+    def initialize(self):
+        self._sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self._sock.bind((self._host, self._port))
+        self._sock.settimeout(4.0)
 
-def send_data_to_clients():
-    time.sleep(10.)
-    while True:
-        resp = ';'.join(cli_data.values()).encode()
-        for client in cli_addr:
-            sock.sendto(resp, client)
-        # time.sleep(0.1)
+    def get_server_url(self):
+        return self._host, self._port
 
-def wait_for_client():
-    print('Start Server...')
-    c_thread = threading.Thread(target=send_data_to_clients,
-                                args=tuple())
-    c_thread.daemon = True
-    c_thread.start()
+    def handle_data(data, from_address):
+        player_id, x, y, z = data.decode().split(':')
+        # print(f'\tPlayer: {player_id} ({x}, {y}, {z})')
+        self._cli_addr.add(from_address)
+        self._cli_data[from_address] = f'{player_id}:{x}:{y}:{z}'
 
-    try:
+    def _send_data_to_clients(self):
+        time.sleep(10.)
         while True:
-            try:
-                data, client_address = sock.recvfrom(1024)
-                # print(f'{client_address} recived')
+            resp = ';'.join(self._cli_data.values()).encode()
+            for client in self._cli_addr:
+                self._sock.sendto(resp, client)
+            # time.sleep(0.1)
 
-                handle_data(data, client_address)
-            except socket.timeout:
-                pass
-    except KeyboardInterrupt:
-        sock.close()
-        print('KeyboardInterrupt catched')
-    print('End...')
+    def wait_for_client(self):
+        print('Start Server...')
+        c_thread = threading.Thread(target=self._send_data_to_clients,
+                                    args=tuple())
+        c_thread.daemon = True
+        c_thread.start()
+
+        try:
+            while True:
+                try:
+                    data, client_address = self._sock.recvfrom(1024)
+                    # print(f'{client_address} recived')
+
+                    handle_data(data, client_address)
+                except socket.timeout:
+                    pass
+        except KeyboardInterrupt:
+            self._sock.close()
+            print('KeyboardInterrupt catched')
+        print('End...')
 
 if __name__ == '__main__':
-    wait_for_client()
+    s = Server('127.0.0.1', 9999)
+    s.initialize()
+    s.wait_for_client()
